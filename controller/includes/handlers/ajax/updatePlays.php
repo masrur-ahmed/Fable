2e107a862e5bdde3ddb1b565a4dfb25f55d9364a
masrur-ahmed <sm.masrur.ahmed@gmail.com>
2020-09-28 12:49:43 +0600

    v1.9

--- controller/includes/handlers/ajax/updatePlays.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("../../connection.php");
-
-if(isset($_POST['audiobookID'])) {
-	$audiobookID = $_POST['audiobookID'];
-
-	$query = mysqli_query($con, "UPDATE audiobook SET plays = plays + 1 WHERE audiobookID='$audiobookID'");
-}
-?>
